17fcaf79beb691a392232a9b624668d1c40178f6
Lam, Doris T (319E) <Doris.T.Lam@jpl.nasa.gov>
2019-11-06 17:04:37 -0800

    fix healthcheck

--- src/main/java/org/openmbee/services/sparql/controllers/HealthCheck.java
+++ src/main/java/org/openmbee/services/sparql/controllers/HealthCheck.java
@@ -1,9 +1,11 @@
 package org.openmbee.services.sparql.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/healthcheck")
+@RestController
+@RequestMapping("/healthcheck")
 public class HealthCheck {
 
     @GetMapping
